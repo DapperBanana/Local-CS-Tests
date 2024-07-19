422f7bc344bce70c8895342a6fbc176d56936a38
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-19 11:57:51 -0600

    Improved error reporting for API endpoints

---
+++ beta_run.cs
@@ -0,0 +1,29 @@
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 10, 20, 5, 35, 30, 15 };
+
+        int largest = int.MinValue;
+        int secondLargest = int.MinValue;
+
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                secondLargest = largest;
+                largest = num;
+            }
+            else if (num > secondLargest && num != largest)
+            {
+                secondLargest = num;
+            }
+        }
+
+        Console.WriteLine("The second largest element in the list is: " + secondLargest);
+    }
+}
