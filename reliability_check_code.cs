a52cab494879724e20c2d6e6bb46533a0c2cf771
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-23 13:28:21 -0600

    Refined data validation for input M

--- reliability_check_code.cs
+++ reliability_check_code.cs
@@ -3,50 +3,36 @@ using System;
 
 class Program
 {
-    static void Main()
+    static string NumberToRoman(int number)
     {
-        Console.Write("Enter a sentence: ");
-        string input = Console.ReadLine();
+        if (number < 1 || number > 3999)
+        {
+            throw new ArgumentOutOfRangeException("number", "Number must be between 1 and 3999");
+        }
 
-        if (IsPalindrome(input))
+        string[] romanNumerals = new string[] { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+        int[] arabicValues = new int[] { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+
+        string romanNumeral = "";
+
+        for (int i = 12; i >= 0; i--)
         {
-            Console.WriteLine("The sentence is a valid palindrome.");
+            while (number >= arabicValues[i])
+            {
+                romanNumeral += romanNumerals[i];
+                number -= arabicValues[i];
+            }
         }
-        else
-        {
-            Console.WriteLine("The sentence is not a valid palindrome.");
-        }
+
+        return romanNumeral;
     }
 
-    static bool IsPalindrome(string input)
+    static void Main()
     {
-        input = input.ToLower(); // Convert input to lowercase
+        Console.Write("Enter a number (1-3999): ");
+        int number = Convert.ToInt32(Console.ReadLine());
 
-        // Remove all non-alphanumeric characters from the input
-        string cleanInput = "";
-        foreach (char c in input)
-        {
-            if (char.IsLetterOrDigit(c))
-            {
-                cleanInput += c;
-            }
-        }
-
-        // Check if the clean input is a palindrome
-        int left = 0;
-        int right = cleanInput.Length - 1;
-
-        while (left < right)
-        {
-            if (cleanInput[left] != cleanInput[right])
-            {
-                return false;
-            }
-
-            left++;
-            right--;
-        }
-
-        return true;
+        string romanNumeral = NumberToRoman(number);
+        Console.WriteLine($"The Roman numeral for {number} is {romanNumeral}");
     }
 }
