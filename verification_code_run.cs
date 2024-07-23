c0c908bebc34cc2555aa04afb463453290330521
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-23 13:30:45 -0600

    Fixed broken links in the application

--- verification_code_run.cs
+++ verification_code_run.cs
@@ -1,24 +1,41 @@
 
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        double radius, height, volume;
+        List<double> list1 = new List<double> { 1, 2, 3, 4, 5 };
+        List<double> list2 = new List<double> { 5, 4, 3, 2, 1 };
 
-        // Get user input for radius
-        Console.Write("Enter the radius of the cylinder: ");
-        radius = Convert.ToDouble(Console.ReadLine());
+        double correlationCoefficient = CalculateCorrelationCoefficient(list1, list2);
+        Console.WriteLine($"Correlation Coefficient: {correlationCoefficient}");
+    }
 
-        // Get user input for height
-        Console.Write("Enter the height of the cylinder: ");
-        height = Convert.ToDouble(Console.ReadLine());
+    static double CalculateCorrelationCoefficient(List<double> list1, List<double> list2)
+    {
+        if (list1.Count != list2.Count)
+        {
+            throw new ArgumentException("Lists must have the same number of elements");
+        }
 
-        // Calculate the volume of the cylinder
-        volume = Math.PI * Math.Pow(radius, 2) * height;
+        double sumX = 0, sumY = 0, sumXSquare = 0, sumYSquare = 0, sumXY = 0;
 
-        // Print the calculated volume
-        Console.WriteLine($"The volume of the cylinder is: {volume}");
+        int n = list1.Count;
+
+        for (int i = 0; i < n; i++)
+        {
+            sumX += list1[i];
+            sumY += list2[i];
+            sumXSquare += Math.Pow(list1[i], 2);
+            sumYSquare += Math.Pow(list2[i], 2);
+            sumXY += list1[i] * list2[i];
+        }
+
+        double numerator = n * sumXY - sumX * sumY;
+        double denominator = Math.Sqrt((n * sumXSquare - Math.Pow(sumX, 2)) * (n * sumYSquare - Math.Pow(sumY, 2));
+
+        return numerator / denominator;
     }
 }
