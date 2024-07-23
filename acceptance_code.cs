5e90cda49d0d434f157c1bfce56ead3eb3bb3b4d
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-23 13:23:15 -0600

    Updated error messages for better user understanding

---
+++ acceptance_code.cs
@@ -0,0 +1,26 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (IsPerfectSquare(num))
+        {
+            Console.WriteLine(num + " is a perfect square.");
+        }
+        else
+        {
+            Console.WriteLine(num + " is not a perfect square.");
+        }
+    }
+
+    static bool IsPerfectSquare(int num)
+    {
+        int sqrt = (int)Math.Sqrt(num);
+        return sqrt * sqrt == num;
+    }
+}
