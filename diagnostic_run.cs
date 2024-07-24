6f28b191a896c1ffd9313197bfba6d74353b296c
Austin L. Howard <austin.lane.howard@gmail.com>
2024-07-24 12:47:09 -0600

    Updated project logo and branding

--- diagnostic_run.cs
+++ diagnostic_run.cs
@@ -7,14 +7,13 @@ class Program
     static void Main()
     {
         List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
-        
+
         int sum = 0;
-        
-        foreach (int number in numbers)
+        foreach (int num in numbers)
         {
-            sum += number;
+            sum += num;
         }
-        
+
         Console.WriteLine("The sum of all elements in the list is: " + sum);
     }
 }
