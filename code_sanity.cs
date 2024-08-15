9ee4fe0e026dfb92db35a2765c604afb236ec38d
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-15 15:59:52 -0600

    Updated project logo and branding

---
+++ code_sanity.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the length of the rectangle: ");
+        double length = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the width of the rectangle: ");
+        double width = double.Parse(Console.ReadLine());
+
+        double area = length * width;
+
+        Console.WriteLine($"The area of the rectangle is: {area}");
+    }
+}
