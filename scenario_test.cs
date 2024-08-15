c6e76791663add1e37f81937815fe2f6d0e4482a
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-15 15:56:18 -0600

    Updated documentation for function X

--- scenario_test.cs
+++ scenario_test.cs
@@ -1,35 +1,59 @@
 
 using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        List<int> list1 = new List<int> { 1, 2, 3, 4, 5 };
-        List<int> list2 = new List<int> { 3, 4, 5, 6, 7 };
+        Console.WriteLine("Welcome to Space Explorer!");
+        Console.WriteLine("You are the captain of a spaceship exploring the galaxy.");
 
-        List<int> commonElements = FindCommonElements(list1, list2);
+        int fuel = 100;
+        int credits = 0;
+        int planetsExplored = 0;
 
-        Console.WriteLine("Common elements between the two lists:");
-        foreach (int element in commonElements)
+        while (true)
         {
-            Console.WriteLine(element);
-        }
-    }
+            Console.WriteLine("\nChoose an action:");
+            Console.WriteLine("1. Explore a new planet");
+            Console.WriteLine("2. Refuel your spaceship");
+            Console.WriteLine("3. Check status");
+            Console.WriteLine("4. Exit");
 
-    static List<int> FindCommonElements(List<int> list1, List<int> list2)
-    {
-        List<int> commonElements = new List<int>();
+            int choice = int.Parse(Console.ReadLine());
 
-        foreach (int element in list1)
-        {
-            if (list2.Contains(element))
+            switch (choice)
             {
-                commonElements.Add(element);
+                case 1:
+                    Console.WriteLine("You explore a new planet...");
+                    fuel -= 20;
+                    credits += 50;
+                    planetsExplored++;
+                    break;
+                case 2:
+                    Console.WriteLine("You refuel your spaceship...");
+                    fuel = 100;
+                    credits -= 30;
+                    break;
+                case 3:
+                    Console.WriteLine("Fuel: " + fuel);
+                    Console.WriteLine("Credits: " + credits);
+                    Console.WriteLine("Planets explored: " + planetsExplored);
+                    break;
+                case 4:
+                    Console.WriteLine("Thanks for playing Space Explorer!");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+
+            if (fuel <= 0)
+            {
+                Console.WriteLine("Your spaceship is out of fuel. Game over!");
+                break;
             }
         }
-
-        return commonElements;
     }
 }
