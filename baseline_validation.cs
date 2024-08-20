aac10dd63d8dc9b69d619671ba16c30198254ef0
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-20 13:58:52 -0600

    Implemented user permission checks in module JJ

---
+++ baseline_validation.cs
@@ -0,0 +1,33 @@
+
+using System;
+
+class Fibonacci
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of terms in the Fibonacci sequence: ");
+        int numTerms = Convert.ToInt32(Console.ReadLine());
+
+        int first = 0, second = 1, next;
+
+        Console.WriteLine("Fibonacci Sequence:");
+
+        for (int i = 0; i < numTerms; i++)
+        {
+            if (i <= 1)
+            {
+                next = i;
+            }
+            else
+            {
+                next = first + second;
+                first = second;
+                second = next;
+            }
+
+            Console.Write(next + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
