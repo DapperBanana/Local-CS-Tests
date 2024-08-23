15676c160cd96dcc4559d06e258d67c33308d4da
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-23 16:23:53 -0600

    Integrated feedback from user testing

--- baseline_validation.cs
+++ baseline_validation.cs
@@ -1,29 +1,21 @@
 
 using System;
+using Newtonsoft.Json;
 
-class FrustumOfConeCalculator
+class Program
 {
     static void Main()
     {
-        double radius1, radius2, height;
+        string input = "{\"name\": \"John\", \"age\": 30}";
         
-        Console.WriteLine("Enter the radius of the base of the frustum: ");
-        radius1 = Convert.ToDouble(Console.ReadLine());
-        
-        Console.WriteLine("Enter the radius of the top of the frustum: ");
-        radius2 = Convert.ToDouble(Console.ReadLine());
-        
-        Console.WriteLine("Enter the height of the frustum: ");
-        height = Convert.ToDouble(Console.ReadLine());
-        
-        double slantHeight = Math.Sqrt(Math.Pow(height, 2) + Math.Pow(radius1 - radius2, 2));
-        
-        double baseArea = Math.PI * Math.Pow(radius1, 2);
-        double topArea = Math.PI * Math.Pow(radius2, 2);
-        double lateralArea = Math.PI * (radius1 + radius2) * slantHeight;
-        
-        double surfaceArea = baseArea + topArea + lateralArea;
-        
-        Console.WriteLine("The surface area of the frustum of a cone is: " + surfaceArea);
+        try
+        {
+            var json = JsonConvert.DeserializeObject(input);
+            Console.WriteLine("Valid JSON");
+        }
+        catch (JsonReaderException)
+        {
+            Console.WriteLine("Invalid JSON");
+        }
     }
 }
