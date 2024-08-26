6cac193773c7f549fe9e0e46ad1835a8f4a286ba
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-26 12:01:27 -0600

    Implemented data caching for faster retrieval

---
+++ baseline_code.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+namespace TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter temperature in Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = (celsius * 9/5) + 32;
+
+            Console.WriteLine($"Temperature in Fahrenheit: {fahrenheit}");
+        }
+    }
+}
