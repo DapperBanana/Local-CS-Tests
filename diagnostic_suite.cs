06c82732d774d70993a41f8ace1d3647a9609f5c
Austin L. Howard <austin.lane.howard@gmail.com>
2024-09-01 12:51:18 -0600

    Fixed memory leaks in feature FF

--- diagnostic_suite.cs
+++ diagnostic_suite.cs
@@ -1,24 +1,28 @@
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        int a = 10;
-        int b = 20;
+        List<int> numbers = new List<int> { 8, 2, 5, 7, 3, 9, 1, 4, 6 };
+        numbers.Sort();
 
-        Console.WriteLine("Before swapping:");
-        Console.WriteLine("a = " + a);
-        Console.WriteLine("b = " + b);
+        double median;
 
-        // Swapping values without using a temporary variable
-        a = a + b;
-        b = a - b;
-        a = a - b;
+        if (numbers.Count % 2 == 0)
+        {
+            // If the list has an even number of elements, average the middle two
+            median = (numbers[numbers.Count / 2 - 1] + numbers[numbers.Count / 2]) / 2.0;
+        }
+        else
+        {
+            // If the list has an odd number of elements, take the middle element
+            median = numbers[numbers.Count / 2];
+        }
 
-        Console.WriteLine("After swapping:");
-        Console.WriteLine("a = " + a);
-        Console.WriteLine("b = " + b);
+        Console.WriteLine($"The median of the list is: {median}");
     }
 }
