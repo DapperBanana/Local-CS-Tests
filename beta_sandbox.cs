d1353b79d06e05cc23bf1de82d9850e49a777c11
Austin L. Howard <austin.lane.howard@gmail.com>
2024-09-11 17:39:51 -0600

    Updated project configuration files

---
+++ beta_sandbox.cs
@@ -0,0 +1,46 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter credit card number: ");
+        string creditCardNumber = Console.ReadLine();
+
+        if (IsValidCreditCardNumber(creditCardNumber))
+        {
+            Console.WriteLine("Valid credit card number.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid credit card number.");
+        }
+    }
+
+    static bool IsValidCreditCardNumber(string creditCardNumber)
+    {
+        int sum = 0;
+        bool isSecondDigit = false;
+
+        for (int i = creditCardNumber.Length - 1; i >= 0; i--)
+        {
+            int digit = creditCardNumber[i] - '0';
+
+            if (isSecondDigit)
+            {
+                digit *= 2;
+
+                if (digit > 9)
+                {
+                    digit = digit % 10 + digit / 10;
+                }
+            }
+
+            sum += digit;
+            isSecondDigit = !isSecondDigit;
+        }
+
+        return sum % 10 == 0;
+    }
+}
