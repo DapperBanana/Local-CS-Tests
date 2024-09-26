414feb4fb351a6dd8ab93f3374261c554fbf785e
Austin L. Howard <austin.lane.howard@gmail.com>
2024-09-26 15:11:36 -0600

    Integrated feedback from usability testing

---
+++ baseline_code_run.cs
@@ -0,0 +1,40 @@
+
+using System;
+
+class PalindromeChecker
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        if (IsPalindrome(input))
+        {
+            Console.WriteLine("The string is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("The string is not a palindrome.");
+        }
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        str = str.ToLower();
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
