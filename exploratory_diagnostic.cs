1a225a09223f2f4f90c427cd5d2bed1a5f94ec82
Austin L. Howard <austin.lane.howard@gmail.com>
2024-09-27 17:01:21 -0600

    Reformatted code for consistency

--- exploratory_diagnostic.cs
+++ exploratory_diagnostic.cs
@@ -1,27 +1,16 @@
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        // Input the three sides of the triangle
-        Console.WriteLine("Enter the length of side 1: ");
-        double side1 = Convert.ToDouble(Console.ReadLine());
-        
-        Console.WriteLine("Enter the length of side 2: ");
-        double side2 = Convert.ToDouble(Console.ReadLine());
-        
-        Console.WriteLine("Enter the length of side 3: ");
-        double side3 = Convert.ToDouble(Console.ReadLine());
-        
-        // Calculate the semiperimeter of the triangle
-        double semiPerimeter = (side1 + side2 + side3) / 2;
+        List<int> numbers = new List<int> { 10, 20, 30, 40, 50 };
 
-        // Calculate the area of the triangle using Heron's formula
-        double area = Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+        double average = numbers.Average();
 
-        // Output the calculated area
-        Console.WriteLine("The area of the triangle is: " + area);
+        Console.WriteLine($"Average: {average}");
     }
 }
