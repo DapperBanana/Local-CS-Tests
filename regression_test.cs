f68376067e4bfeca7b950560821dea803610104e
Austin L. Howard <austin.lane.howard@gmail.com>
2024-09-29 12:25:27 -0600

    Synchronized data models with API changes

---
+++ regression_test.cs
@@ -0,0 +1,69 @@
+
+using System;
+
+class MatrixOperations
+{
+    static void Main()
+    {
+        int[,] matrix1 = { { 1, 2 }, { 3, 4 } };
+        int[,] matrix2 = { { 5, 6 }, { 7, 8 } };
+
+        int[,] resultAddition = AddMatrices(matrix1, matrix2);
+        int[,] resultSubtraction = SubtractMatrices(matrix1, matrix2);
+
+        Console.WriteLine("Matrix Addition:");
+        PrintMatrix(resultAddition);
+        
+        Console.WriteLine("\nMatrix Subtraction:");
+        PrintMatrix(resultSubtraction);
+    }
+
+    static int[,] AddMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows = matrix1.GetLength(0);
+        int cols = matrix1.GetLength(1);
+        int[,] result = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+
+        return result;
+    }
+
+    static int[,] SubtractMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows = matrix1.GetLength(0);
+        int cols = matrix1.GetLength(1);
+        int[,] result = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result[i, j] = matrix1[i, j] - matrix2[i, j];
+            }
+        }
+
+        return result;
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
