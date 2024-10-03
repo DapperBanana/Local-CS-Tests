c2bba76e37f85455bb1f4e6d680b8280cb126773
Austin L. Howard <austin.lane.howard@gmail.com>
2024-10-03 11:42:46 -0600

    Fixed broken unit tests in module HH

---
+++ sample_run.cs
@@ -0,0 +1,16 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double edgeLength;
+        Console.Write("Enter the edge length of the tetrahedron: ");
+        edgeLength = double.Parse(Console.ReadLine());
+
+        double area = Math.Sqrt(3) * Math.Pow(edgeLength, 2);
+        
+        Console.WriteLine("The area of the regular tetrahedron is: " + area);
+    }
+}
