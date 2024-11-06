4064baed3db81140a8351145150cd17fa62829d9
Austin L. Howard <austin.lane.howard@gmail.com>
2024-11-05 18:37:10 -0700

    Integrated code coverage analysis tools

---
+++ preliminary_validation.cs
@@ -0,0 +1,35 @@
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> list1 = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> list2 = new List<int> { 3, 4, 5, 6, 7 };
+
+        List<int> commonElements = FindCommonElements(list1, list2);
+
+        Console.WriteLine("Common elements between list1 and list2:");
+        foreach (int element in commonElements)
+        {
+            Console.WriteLine(element);
+        }
+    }
+
+    static List<int> FindCommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> commonElements = new List<int>();
+
+        foreach (int element in list1)
+        {
+            if (list2.Contains(element))
+            {
+                commonElements.Add(element);
+            }
+        }
+
+        return commonElements;
+    }
+}
