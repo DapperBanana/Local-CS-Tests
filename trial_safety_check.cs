ce2a9fb59121184051c809fed6581422cc9d5dba
Austin L. Howard <austin.lane.howard@gmail.com>
2024-11-07 12:10:23 -0700

    Updated configuration settings for environment I

--- trial_safety_check.cs
+++ trial_safety_check.cs
@@ -3,19 +3,41 @@ using System;
 
 class Program
 {
+    static bool IsPrime(int num)
+    {
+        if (num < 2)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static int SumOfPrimesInRange(int start, int end)
+    {
+        int sum = 0;
+        for (int i = start; i <= end; i++)
+        {
+            if (IsPrime(i))
+                sum += i;
+        }
+
+        return sum;
+    }
+
     static void Main()
     {
-        string str = "Hello, World!";
-        string reversedStr = ReverseString(str);
-        
-        Console.WriteLine("Original string: " + str);
-        Console.WriteLine("Reversed string: " + reversedStr);
-    }
-    
-    static string ReverseString(string str)
-    {
-        char[] charArray = str.ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
+        Console.WriteLine("Enter the range start: ");
+        int start = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the range end: ");
+        int end = int.Parse(Console.ReadLine());
+
+        int sum = SumOfPrimesInRange(start, end);
+
+        Console.WriteLine($"The sum of all prime numbers in the range {start}-{end} is: {sum}");
     }
 }
