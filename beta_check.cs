192afed4239abde3c1b15ba8b0f3da8931c83d1c
Austin L. Howard <austin.lane.howard@gmail.com>
2024-11-10 17:09:51 -0700

    Implemented caching strategy for performance gains

--- beta_check.cs
+++ beta_check.cs
@@ -1,33 +1,76 @@
 
 using System;
+using System.Collections.Generic;
+
+class Graph
+{
+    private int V;
+    private List<int>[] adj;
+
+    public Graph(int v)
+    {
+        V = v;
+        adj = new List<int>[v];
+        for (int i = 0; i < v; i++)
+        {
+            adj[i] = new List<int>();
+        }
+    }
+
+    public void AddEdge(int u, int v)
+    {
+        adj[u].Add(v);
+        adj[v].Add(u);
+    }
+
+    private bool IsCyclicUtil(int v, bool[] visited, int parent)
+    {
+        visited[v] = true;
+
+        foreach (int i in adj[v])
+        {
+            if (!visited[i])
+            {
+                if (IsCyclicUtil(i, visited, v))
+                    return true;
+            }
+            else if (i != parent)
+                return true;
+        }
+
+        return false;
+    }
+
+    public bool IsTree()
+    {
+        bool[] visited = new bool[V];
+
+        if (IsCyclicUtil(0, visited, -1))
+            return false;
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!visited[i])
+                return false;
+        }
+
+        return true;
+    }
+}
 
 class Program
 {
     static void Main()
     {
-        string[] choices = { "rock", "paper", "scissors" };
+        Graph g = new Graph(5);
+        g.AddEdge(0, 1);
+        g.AddEdge(0, 2);
+        g.AddEdge(0, 3);
+        g.AddEdge(1, 4);
 
-        Random rand = new Random();
-        int computerChoice = rand.Next(0, 3);
-
-        Console.WriteLine("Choose rock, paper, or scissors:");
-        string userChoice = Console.ReadLine().ToLower();
-
-        Console.WriteLine("Computer chooses: " + choices[computerChoice]);
-
-        if (userChoice == choices[computerChoice])
-        {
-            Console.WriteLine("It's a tie!");
-        }
-        else if ((userChoice == "rock" && choices[computerChoice] == "scissors") ||
-                 (userChoice == "paper" && choices[computerChoice] == "rock") ||
-                 (userChoice == "scissors" && choices[computerChoice] == "paper"))
-        {
-            Console.WriteLine("You win!");
-        }
+        if (g.IsTree())
+            Console.WriteLine("Graph is a tree");
         else
-        {
-            Console.WriteLine("You lose!");
-        }
+            Console.WriteLine("Graph is not a tree");
     }
 }
