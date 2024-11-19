f6477fb88e40e1ccaaf5df9d9aa69295f5f94818
Austin L. Howard <austin.lane.howard@gmail.com>
2024-11-19 14:44:13 -0700

    Merged feature branch N into main

--- trial_script.cs
+++ trial_script.cs
@@ -1,47 +1,67 @@
 
 using System;
-using System.Collections.Generic;
-using System.Windows.Forms.DataVisualization.Charting;
 
-namespace LineChartGenerator
+class Program
 {
-    class Program
+    static bool IsValidISBN(string isbn)
     {
-        static void Main(string[] args)
+        // Remove any hyphens from the input string
+        isbn = isbn.Replace("-", "");
+
+        // Check that the ISBN is either 10 or 13 characters long
+        if (isbn.Length != 10 && isbn.Length != 13)
         {
-            // Sample data
-            Dictionary<string, int> data = new Dictionary<string, int>()
+            return false;
+        }
+
+        // Compute the check digit for ISBN-10
+        if (isbn.Length == 10)
+        {
+            int sum = 0;
+            for (int i = 0; i < 9; i++)
             {
-                { "Jan", 10 },
-                { "Feb", 20 },
-                { "Mar", 15 },
-                { "Apr", 25 },
-                { "May", 30 }
-            };
-
-            // Create a new chart
-            Chart chart = new Chart();
-            chart.Size = new System.Drawing.Size(800, 600);
-
-            // Add chart area
-            ChartArea chartArea = new ChartArea();
-            chart.ChartAreas.Add(chartArea);
-
-            // Add series
-            Series series = new Series();
-            series.ChartType = SeriesChartType.Line;
-            chart.Series.Add(series);
-
-            // Add data points from dictionary
-            foreach (var item in data)
-            {
-                series.Points.AddXY(item.Key, item.Value);
+                sum += (i + 1) * int.Parse(isbn[i].ToString());
             }
 
-            // Show the chart
-            Form form = new Form();
-            form.Controls.Add(chart);
-            form.ShowDialog();
+            int checkDigit = sum % 11;
+            if (checkDigit == 10)
+            {
+                return isbn[9] == 'X';
+            }
+            else
+            {
+                return int.Parse(isbn[9].ToString()) == checkDigit;
+            }
+        }
+
+        // Compute the check digit for ISBN-13
+        if (isbn.Length == 13)
+        {
+            int sum = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                sum += int.Parse(isbn[i].ToString()) * (i % 2 == 0 ? 1 : 3);
+            }
+
+            int checkDigit = (10 - (sum % 10)) % 10;
+            return int.Parse(isbn[12].ToString()) == checkDigit;
+        }
+
+        return false;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter an ISBN number: ");
+        string isbn = Console.ReadLine();
+
+        if (IsValidISBN(isbn))
+        {
+            Console.WriteLine("The ISBN number is valid.");
+        }
+        else
+        {
+            Console.WriteLine("The ISBN number is invalid.");
         }
     }
 }
