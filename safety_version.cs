838f8105cd0b84fa934feb2e3dfeff9fa262d577
Austin L. Howard <austin.lane.howard@gmail.com>
2024-11-19 14:39:05 -0700

    Implemented automated deployment scripts

---
+++ safety_version.cs
@@ -0,0 +1,35 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsPalindrome(number))
+        {
+            Console.WriteLine(number + " is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine(number + " is not a palindrome.");
+        }
+    }
+
+    static bool IsPalindrome(int number)
+    {
+        int reversedNumber = 0;
+        int originalNumber = number;
+
+        while (number > 0)
+        {
+            int remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+}
