77665a12558020fd78f77fb4afb63c87937465ee
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-13 11:32:58 -0700

    Fixed edge case scenario in module U

--- system_run.cs
+++ system_run.cs
@@ -1,50 +1,17 @@
 
 using System;
+using System.Text.RegularExpressions;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Enter a list of numbers separated by spaces:");
-        string input = Console.ReadLine();
+        string inputString = "Hello, World! This is a test string.";
 
-        string[] numbers = input.Split(' ');
-        int[] arr = new int[numbers.Length];
+        // Remove all non-alphanumeric characters from the input string
+        string resultString = Regex.Replace(inputString, @"[^\w\s]", "");
 
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            arr[i] = int.Parse(numbers[i]);
-        }
-
-        int result = GetLCM(arr);
-        Console.WriteLine($"The Least Common Multiple is: {result}");
-    }
-
-    static int GetLCM(int[] numbers)
-    {
-        int lcm = numbers[0];
-
-        for (int i = 1; i < numbers.Length; i++)
-        {
-            lcm = GetLCMOfTwo(lcm, numbers[i]);
-        }
-
-        return lcm;
-    }
-
-    static int GetLCMOfTwo(int a, int b)
-    {
-        return a * b / GetGCD(a, b);
-    }
-
-    static int GetGCD(int a, int b)
-    {
-        while (b != 0)
-        {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+        Console.WriteLine("Input string: " + inputString);
+        Console.WriteLine("String without punctuation: " + resultString);
     }
 }
