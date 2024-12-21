e198d5634b1da18ede4cee60039cbef825e09aac
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-21 16:08:50 -0700

    Updated UI components for a more modern look

---
+++ regression_run.cs
@@ -0,0 +1,21 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> list1 = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> list2 = new List<int> { 3, 4, 5, 6, 7 };
+
+        var commonElements = list1.Intersect(list2);
+
+        Console.WriteLine("Common elements between list1 and list2:");
+        foreach (var element in commonElements)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
