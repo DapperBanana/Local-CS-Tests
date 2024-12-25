c402b4d03799cd61ff84e59f8e72004e834b3939
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-25 15:56:52 -0700

    Integrated unit test coverage for critical paths

---
+++ script_check.cs
@@ -0,0 +1,25 @@
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+            
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            else
+            {
+                Console.WriteLine("Failed to fetch data from API. Status code: " + response.StatusCode);
+            }
+        }
+    }
+}
