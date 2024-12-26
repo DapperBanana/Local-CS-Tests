e7c67be267f0de4e8f41eaed8ca4a9e7729fc157
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-26 11:33:12 -0700

    Fixed memory leaks in feature FF

--- unit_test.cs
+++ unit_test.cs
@@ -1,41 +1,41 @@
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class Program
 {
-    static void Main()
+    static bool IsPrime(int number)
     {
-        List<double> list1 = new List<double> { 1, 2, 3, 4, 5 };
-        List<double> list2 = new List<double> { 2, 4, 6, 8, 10 };
-
-        double correlationCoefficient = CalculateCorrelationCoefficient(list1, list2);
+        if (number <= 1)
+            return false;
         
-        Console.WriteLine("Correlation Coefficient: " + correlationCoefficient);
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        
+        return true;
     }
 
-    static double CalculateCorrelationCoefficient(List<double> list1, List<double> list2)
+    static int FindNextPrime(int number)
     {
-        if (list1.Count != list2.Count)
+        int next = number + 1;
+
+        while (true)
         {
-            throw new ArgumentException("Lists must have the same length");
+            if (IsPrime(next))
+                return next;
+
+            next++;
         }
+    }
 
-        double sumList1 = list1.Sum();
-        double sumList2 = list2.Sum();
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int input = Convert.ToInt32(Console.ReadLine());
 
-        double sumList1Squared = list1.Sum(x => x * x);
-        double sumList2Squared = list2.Sum(x => x * x);
-
-        double sumProduct = list1.Zip(list2, (x, y) => x * y).Sum();
-
-        int n = list1.Count;
-
-        double correlationCoefficient = (n * sumProduct - sumList1 * sumList2) /
-                                        Math.Sqrt((n * sumList1Squared - Math.Pow(sumList1, 2)) * 
-                                                  (n * sumList2Squared - Math.Pow(sumList2, 2)));
-
-        return correlationCoefficient;
-  }
+        int nextPrime = FindNextPrime(input);
+        Console.WriteLine($"The smallest prime number greater than {input} is: {nextPrime}");
+    }
 }
