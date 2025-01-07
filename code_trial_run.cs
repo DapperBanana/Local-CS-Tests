d8efc897140f906faf0f84cda6f6c9154f93a3fd
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-06 18:28:36 -0700

    Optimized database schema for improved efficiency

--- code_trial_run.cs
+++ code_trial_run.cs
@@ -1,17 +1,54 @@
 
 using System;
-using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        string str1 = "hello";
-        string str2 = "world";
+        string[] choices = { "rock", "paper", "scissors" };
+        Random random = new Random();
 
-        var commonChars = str1.Intersect(str2);
+        Console.WriteLine("Welcome to Rock-Paper-Scissors game!");
 
-        Console.WriteLine("Common characters between '{0}' and '{1}': {2}",
-            str1, str2, string.Join(", ", commonChars));
+        while (true)
+        {
+            Console.WriteLine("\nEnter your choice (rock, paper, scissors): ");
+            string userChoice = Console.ReadLine().ToLower();
+
+            if (userChoice != "rock" && userChoice != "paper" && userChoice != "scissors")
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+                continue;
+            }
+
+            int computerIndex = random.Next(choices.Length);
+            string computerChoice = choices[computerIndex];
+
+            Console.WriteLine($"Computer chooses: {computerChoice}");
+
+            if (userChoice == computerChoice)
+            {
+                Console.WriteLine("It's a tie!");
+            }
+            else if ((userChoice == "rock" && computerChoice == "scissors") ||
+                     (userChoice == "paper" && computerChoice == "rock") ||
+                     (userChoice == "scissors" && computerChoice == "paper"))
+            {
+                Console.WriteLine("You win!");
+            }
+            else
+            {
+                Console.WriteLine("Computer wins!");
+            }
+
+            Console.WriteLine("Play again? (yes/no)");
+            string playAgain = Console.ReadLine().ToLower();
+
+            if (playAgain != "yes")
+            {
+                Console.WriteLine("Thanks for playing. Goodbye!");
+                break;
+            }
+        }
     }
 }
