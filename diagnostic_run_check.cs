67491b2e77250def1553229188ff57d83479b90c
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-10 17:23:07 -0700

    Refactored code to follow design patterns in module BB

---
+++ diagnostic_run_check.cs
@@ -0,0 +1,87 @@
+
+using System;
+
+class MagicSquareChecker
+{
+    public static bool IsMagicSquare(int[,] matrix)
+    {
+        int n = matrix.GetLength(0);
+        int sum = 0;
+        
+        // Calculate the sum of the first row
+        for (int j = 0; j < n; j++)
+        {
+            sum += matrix[0, j];
+        }
+        
+        // Check the sum of each row
+        for (int i = 1; i < n; i++)
+        {
+            int rowSum = 0;
+            for (int j = 0; j < n; j++)
+            {
+                rowSum += matrix[i, j];
+            }
+            if (rowSum != sum)
+            {
+                return false;
+            }
+        }
+        
+        // Check the sum of each column
+        for (int j = 0; j < n; j++)
+        {
+            int colSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                colSum += matrix[i, j];
+            }
+            if (colSum != sum)
+            {
+                return false;
+            }
+        }
+        
+        // Check the sum of the main diagonal
+        int diagSum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            diagSum += matrix[i, i];
+        }
+        if (diagSum != sum)
+        {
+            return false;
+        }
+        
+        // Check the sum of the secondary diagonal
+        int secDiagSum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            secDiagSum += matrix[i, n - 1 - i];
+        }
+        if (secDiagSum != sum)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+
+    static void Main()
+    {
+        int[,] matrix = {
+            {2, 7, 6},
+            {9, 5, 1},
+            {4, 3, 8}
+        };
+
+        if (IsMagicSquare(matrix))
+        {
+            Console.WriteLine("The given matrix is a magic square.");
+        }
+        else
+        {
+            Console.WriteLine("The given matrix is not a magic square.");
+        }
+    }
+}
