95c562cd9b8767cee2c7739e867d0b0677fc9757
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-12 14:49:53 -0700

    Removed deprecated function H

---
+++ script_sanity.cs
@@ -0,0 +1,53 @@
+
+using System;
+
+class Program
+{
+    static bool IsOrthogonalMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (rows != cols)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                int dotProduct = 0;
+                for (int k = 0; k < rows; k++)
+                {
+                    dotProduct += matrix[i, k] * matrix[j, k];
+                }
+
+                if ((i != j && dotProduct != 0) || (i == j && dotProduct != 1))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        int[,] matrix = {
+            {1, 0, 0},
+            {0, 1, 0},
+            {0, 0, 1}
+        };
+
+        if (IsOrthogonalMatrix(matrix))
+        {
+            Console.WriteLine("The given matrix is orthogonal.");
+        }
+        else
+        {
+            Console.WriteLine("The given matrix is not orthogonal.");
+        }
+    }
+}
