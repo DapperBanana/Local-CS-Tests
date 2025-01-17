2c478e6982ca800160d0b732c2109017cc9f3d1a
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-16 19:02:01 -0700

    Fixed accessibility issues in the UI

---
+++ prototype_test.cs
@@ -0,0 +1,74 @@
+
+using System;
+
+class MatrixOperations
+{
+    static void Main()
+    {
+        int[,] matrix1 = { { 1, 2, 3 }, { 4, 5, 6 } };
+        int[,] matrix2 = { { 7, 8, 9 }, { 10, 11, 12 } };
+
+        Console.WriteLine("Matrix 1:");
+        PrintMatrix(matrix1);
+
+        Console.WriteLine("Matrix 2:");
+        PrintMatrix(matrix2);
+
+        int[,] resultAddition = AddMatrices(matrix1, matrix2);
+        Console.WriteLine("Matrix Addition:");
+        PrintMatrix(resultAddition);
+
+        int[,] resultSubtraction = SubtractMatrices(matrix1, matrix2);
+        Console.WriteLine("Matrix Subtraction:");
+        PrintMatrix(resultSubtraction);
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int[,] AddMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows = matrix1.GetLength(0);
+        int cols = matrix1.GetLength(1);
+        int[,] result = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        }
+
+        return result;
+    }
+
+    static int[,] SubtractMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows = matrix1.GetLength(0);
+        int cols = matrix1.GetLength(1);
+        int[,] result = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result[i, j] = matrix1[i, j] - matrix2[i, j];
+            }
+        }
+
+        return result;
+    }
+}
