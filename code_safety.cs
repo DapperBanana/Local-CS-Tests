507466149ef54ca7788eb07553cde0e037de5273
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-28 12:29:37 -0700

    Applied coding standards to maintain consistency

---
+++ code_safety.cs
@@ -0,0 +1,27 @@
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        int sum = GetSum(numbers);
+
+        Console.WriteLine("Sum of all elements in the list: " + sum);
+    }
+
+    static int GetSum(List<int> numbers)
+    {
+        int sum = 0;
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
