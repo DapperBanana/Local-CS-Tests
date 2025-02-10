82f3ddce6f25f02ce1de133dca99543dda782064
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-10 13:07:05 -0700

    Integrated support for push notifications

---
+++ diagnostic_test.cs
@@ -0,0 +1,27 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number to calculate its factorial: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        long result = Factorial(num);
+        
+        Console.WriteLine($"Factorial of {num} is: {result}");
+    }
+
+    static long Factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+    }
+}
