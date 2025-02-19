def04a077b82537cdf73cc2ed1e22452372af1ab
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-19 11:55:04 -0700

    Implemented API rate limiting for security

--- script_sandbox.cs
+++ script_sandbox.cs
@@ -1,49 +1,46 @@
 
 using System;
-using System.Text.RegularExpressions;
 
-class Program
+public class VampireNumberChecker
 {
-    static void Main()
+    public static bool IsVampireNumber(int number)
     {
-        Console.Write("Enter the ISBN number: ");
-        string isbn = Console.ReadLine();
-
-        if (IsValidISBN(isbn))
+        string numberString = number.ToString();
+        int numDigits = numberString.Length;
+        
+        if (numDigits % 2 != 0)
         {
-            Console.WriteLine("The entered ISBN number is valid.");
+            return false; // Vampire number must have an even number of digits
+        }
+        
+        for (int i = (int)Math.Pow(10, numDigits/2 - 1); i < Math.Sqrt(number) + 1; i++)
+        {
+            if (number % i == 0)
+            {
+                int j = number / i;
+                string productString = (i.ToString() + j.ToString());
+                
+                if (String.Concat(numberString.OrderBy(c => c)) == String.Concat(productString.OrderBy(c => c)))
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    public static void Main()
+    {
+        int input = 1827;
+        
+        if (IsVampireNumber(input))
+        {
+            Console.WriteLine(input + " is a vampire number.");
         }
         else
         {
-            Console.WriteLine("The entered ISBN number is invalid.");
+            Console.WriteLine(input + " is not a vampire number.");
         }
-    }
-
-    static bool IsValidISBN(string isbn)
-    {
-        // Remove any dashes or spaces from the ISBN
-        isbn = isbn.Replace("-", "").Replace(" ", "");
-
-        // Check if the ISBN is 10 or 13 characters long
-        if (isbn.Length != 10 && isbn.Length != 13) return false;
-
-        // Check if the ISBN contains only digits and the last character can be 'X' for ISBN-10
-        if (!Regex.IsMatch(isbn, "^[0-9X]+$")) return false;
-
-        // Calculate the ISBN checksum
-        int checksum = 0;
-        for (int i = 0; i < isbn.Length - 1; i++)
-        {
-            int digit = isbn[i] == 'X' ? 10 : (int)char.GetNumericValue(isbn[i]);
-            checksum += digit * (isbn.Length - i);
-        }
-
-        // For ISBN-10, the checksum should be divisible by 11
-        if (isbn.Length == 10 && checksum % 11 != 0) return false;
-
-        // For ISBN-13, the checksum should be divisible by 10
-        if (isbn.Length == 13 && checksum % 10 != 0) return false;
-
-        return true;
     }
 }
