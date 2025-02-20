c74bc6344a190a3e646474844ba18fbc18e4464d
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-20 16:57:12 -0700

    Optimized SQL queries for faster execution

--- trial_sandbox.cs
+++ trial_sandbox.cs
@@ -5,33 +5,15 @@ class Program
 {
     static void Main()
     {
-        string[] movies = { "The Shawshank Redemption", "The Godfather", "The Dark Knight", "Pulp Fiction", "Schindler's List" };
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Welcome to the Movie Recommendation System!");
-        Console.WriteLine("Here are some popular movies:");
-
-        for (int i = 0; i < movies.Length; i++)
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
         {
-            Console.WriteLine($"{i + 1}. {movies[i]}");
+            sum += i;
         }
 
-        Console.WriteLine("Enter the number of the movie you like:");
-        int userInput = Convert.ToInt32(Console.ReadLine());
-
-        if(userInput >= 1 && userInput <= movies.Length)
-        {
-            Console.WriteLine($"You might also like these movies: ");
-            foreach (string movie in movies)
-            {
-                if (movie != movies[userInput - 1])
-                {
-                    Console.WriteLine(movie);
-                }
-            }
-        }
-        else
-        {
-            Console.WriteLine("Invalid input. Please enter a number between 1 and " + movies.Length);
-        }
+        Console.WriteLine("The sum of the first " + n + " natural numbers is: " + sum);
     }
 }
