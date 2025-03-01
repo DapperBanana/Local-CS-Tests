b574871a1b1a40b35ca8fce67b4b79a6b6f78fcf
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-01 16:59:01 -0700

    Improved error reporting for API endpoints

---
+++ verification_code.cs
@@ -0,0 +1,28 @@
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> list1 = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> list2 = new List<int> { 3, 4, 5, 6, 7 };
+
+        List<int> commonElements = new List<int>();
+
+        foreach (int element in list1)
+        {
+            if (list2.Contains(element))
+            {
+                commonElements.Add(element);
+            }
+        }
+
+        Console.WriteLine("Common elements between the two lists:");
+        foreach (int element in commonElements)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
