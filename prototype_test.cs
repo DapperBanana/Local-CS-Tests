e0290b64e0c76a89447353c27877c0c21675a03c
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-09 18:42:45 -0600

    Optimized database schema for improved efficiency

---
+++ prototype_test.cs
@@ -0,0 +1,34 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str1 = "karolin";
+        string str2 = "kathrin";
+
+        int hammingDistance = CalculateHammingDistance(str1, str2);
+
+        Console.WriteLine($"Hamming distance between {str1} and {str2} is: {hammingDistance}");
+    }
+
+    static int CalculateHammingDistance(string str1, string str2)
+    {
+        if (str1.Length != str2.Length)
+        {
+            throw new ArgumentException("Strings must be of equal length");
+        }
+
+        int distance = 0;
+        for (int i = 0; i < str1.Length; i++)
+        {
+            if (str1[i] != str2[i])
+            {
+                distance++;
+            }
+        }
+
+        return distance;
+    }
+}
