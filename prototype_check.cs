9c3d13076694f99d4cd9639a040c24fd42050216
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-15 16:59:51 -0600

    Updated project dependencies for new features

---
+++ prototype_check.cs
@@ -0,0 +1,37 @@
+
+using System;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        string xmlString = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE note [
+  <!ELEMENT note (to,from,heading,body)>
+  <!ELEMENT to (#PCDATA)>
+  <!ELEMENT from (#PCDATA)>
+  <!ELEMENT heading (#PCDATA)>
+  <!ELEMENT body (#PCDATA)>
+]>
+<note>
+  <to>Tove</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+</note>";
+
+        try
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.XmlResolver = null; // Disable DTD processing to prevent external entity injection
+            doc.LoadXml(xmlString);
+
+            Console.WriteLine("The given string is a valid XML document with DTD.");
+        }
+        catch (XmlException)
+        {
+            Console.WriteLine("The given string is not a valid XML document with DTD.");
+        }
+    }
+}
