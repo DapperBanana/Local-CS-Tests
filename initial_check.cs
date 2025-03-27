1e1f9e72330d55940d88658e0c5a68f0ca04bded
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-27 14:51:22 -0600

    Implemented API rate limiting for security

---
+++ initial_check.cs
@@ -0,0 +1,27 @@
+
+using System;
+
+class PrimeFactors
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("Prime factors of " + number + " are: ");
+        
+        for (int i = 2; i <= number; i++)
+        {
+            while (number % i == 0)
+            {
+                Console.Write(i + " ");
+                number = number / i;
+            }
+        }
+
+        if (number > 2)
+        {
+            Console.Write(number);
+        }
+    }
+}
