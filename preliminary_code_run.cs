589cab23bc4c5ce3a53a945eb71cedad0a8e4f06
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-05 18:38:03 -0600

    Implemented user authentication for improved security

--- preliminary_code_run.cs
+++ preliminary_code_run.cs
@@ -1,32 +1,40 @@
 
 using System;
-using System.Linq;
 
 class Program
 {
+    static bool IsPrime(int num)
+    {
+        if (num <= 1) return false;
+        if (num == 2 || num == 3) return true;
+        if (num % 2 == 0) return false;
+
+        for (int i = 3; i <= Math.Sqrt(num); i += 2)
+        {
+            if (num % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    static int NextPrime(int num)
+    {
+        num++;
+
+        while (!IsPrime(num))
+        {
+            num++;
+        }
+
+        return num;
+    }
+
     static void Main()
     {
-        Console.WriteLine("Enter the numbers separated by spaces:");
-        string input = Console.ReadLine();
+        Console.Write("Enter a number: ");
+        int input = int.Parse(Console.ReadLine());
 
-        // Convert the input string to an array of numbers
-        int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
-
-        // Sort the array
-        Array.Sort(numbers);
-
-        // Find the median
-        double median;
-        if (numbers.Length % 2 == 0)
-        {
-            median = (double)(numbers[numbers.Length / 2 - 1] + numbers[numbers.Length / 2]) / 2;
-        }
-        else
-        {
-            median = numbers[numbers.Length / 2];
-        }
-
-        // Display the median
-        Console.WriteLine($"The median is: {median}");
+        int result = NextPrime(input);
+        Console.WriteLine($"The smallest prime number greater than {input} is: {result}");
     }
 }
