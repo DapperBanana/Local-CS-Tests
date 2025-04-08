b9c5f6093f52ff2d2fb108974871e6ca065772e7
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-08 12:02:00 -0600

    Added missing input validation for module DD

---
+++ practice_test.cs
@@ -0,0 +1,40 @@
+
+using System;
+
+class Program
+{
+    static bool IsPrime(int num)
+    {
+        if (num <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int nextPrime = num + 1;
+        while (true)
+        {
+            if (IsPrime(nextPrime))
+            {
+                Console.WriteLine($"The smallest prime number greater than {num} is: {nextPrime}");
+                break;
+            }
+            nextPrime++;
+        }
+    }
+}
