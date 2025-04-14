a6eb5dc014ebb48d35a3b8adcca50245865a1481
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-14 15:29:46 -0600

    Updated dependencies to version C

---
+++ exploratory_safety.cs
@@ -0,0 +1,21 @@
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string str1 = "hello";
+        string str2 = "world";
+
+        var commonChars = str1.Intersect(str2);
+
+        Console.WriteLine("Common characters between the two strings:");
+        foreach (var c in commonChars)
+        {
+            Console.Write(c + " ");
+        }
+
+    }
+}
