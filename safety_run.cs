9d18018738507314cf32c12bfde13f9a29cc3496
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-30 12:16:31 -0600

    Added new feature Z

---
+++ safety_run.cs
@@ -0,0 +1,23 @@
+
+using System;
+
+namespace SwapValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+
+            Console.WriteLine($"Before swapping - a: {a}, b: {b}");
+
+            // Swapping without using a temporary variable
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"After swapping - a: {a}, b: {b}");
+        }
+    }
+}
