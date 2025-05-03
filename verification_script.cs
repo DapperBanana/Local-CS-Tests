dd1ab658ed5be7791a07afdad156c1248c441c50
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-03 13:43:39 -0600

    Applied coding standards to maintain consistency

---
+++ verification_script.cs
@@ -0,0 +1,31 @@
+
+using System;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        string xmlString = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE note SYSTEM ""note.dtd"">
+<note>
+    <to>Tove</to>
+    <from>Jani</from>
+    <heading>Reminder</heading>
+    <body>Don't forget me this weekend!</body>
+</note>";
+
+        try
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.XmlResolver = null;
+            xmlDocument.LoadXml(xmlString);
+
+            Console.WriteLine("The given string is a valid XML document with a DTD.");
+        }
+        catch (XmlException)
+        {
+            Console.WriteLine("The given string is not a valid XML document with a DTD.");
+        }
+    }
+}
