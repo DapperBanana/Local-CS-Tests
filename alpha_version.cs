746fd0501c58bf96ed68b21af179dbdfa29100e2
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-19 12:22:26 -0600

    Improved logging for better debugging

---
+++ alpha_version.cs
@@ -0,0 +1,40 @@
+
+using System;
+
+class Program
+{
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+            return false;
+        
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        
+        return true;
+    }
+    
+    static int NextPrime(int number)
+    {
+        number++;
+        while(!IsPrime(number))
+        {
+            number++;
+        }
+        
+        return number;
+    }
+    
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int input = Convert.ToInt32(Console.ReadLine());
+        
+        int nextPrime = NextPrime(input);
+        
+        Console.WriteLine($"The smallest prime number greater than {input} is: {nextPrime}");
+    }
+}
