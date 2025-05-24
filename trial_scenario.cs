1ed3981ba21b92e393d6dc76456814c694f46528
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-24 17:50:24 -0600

    Merged feature branch N into main

--- trial_scenario.cs
+++ trial_scenario.cs
@@ -1,87 +1,35 @@
 
 using System;
 
-class PersonalityQuiz
+class Program
 {
     static void Main()
     {
-        Console.WriteLine("Welcome to the Personality Quiz!");
-        
-        int score = 0;
+        Console.Write("Enter a decimal number: ");
+        double number = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Question 1: What is your favorite color?");
-        Console.WriteLine("a) Blue");
-        Console.WriteLine("b) Red");
-        Console.WriteLine("c) Green");
-
-        string answer1 = Console.ReadLine();
-        
-        if (answer1 == "a")
+        // Convert decimal to fraction
+        double epsilon = 0.000001;
+        int maxIterations = 10000;
+        int n = 1;
+        double error = double.MaxValue;
+        double fraction = 0;
+        for (int i = 1; i <= maxIterations; i++)
         {
-            score += 1;
-        }
-        else if (answer1 == "b")
-        {
-            score += 2;
-        }
-        else if (answer1 == "c")
-        {
-            score += 3;
-        }
-
-        Console.WriteLine("Question 2: What is your favorite animal?");
-        Console.WriteLine("a) Dog");
-        Console.WriteLine("b) Cat");
-        Console.WriteLine("c) Bird");
-
-        string answer2 = Console.ReadLine();
-        
-        if (answer2 == "a")
-        {
-            score += 1;
-        }
-        else if (answer2 == "b")
-        {
-            score += 2;
-        }
-        else if (answer2 == "c")
-        {
-            score += 3;
+            double tempFraction = Math.Round(number * i) / i;
+            double tempError = Math.Abs(number - tempFraction);
+            if (tempError < error)
+            {
+                error = tempError;
+                fraction = tempFraction;
+                n = i;
+            }
+            if (error < epsilon)
+            {
+                break;
+            }
         }
 
-        Console.WriteLine("Question 3: What is your favorite hobby?");
-        Console.WriteLine("a) Reading");
-        Console.WriteLine("b) Playing sports");
-        Console.WriteLine("c) Drawing");
-
-        string answer3 = Console.ReadLine();
-        
-        if (answer3 == "a")
-        {
-            score += 1;
-        }
-        else if (answer3 == "b")
-        {
-            score += 2;
-        }
-        else if (answer3 == "c")
-        {
-            score += 3;
-        }
-
-        Console.WriteLine("Your personality type is: ");
-
-        if (score <= 3)
-        {
-            Console.WriteLine("You are introverted and enjoy quiet activities.");
-        }
-        else if (score <= 6)
-        {
-            Console.WriteLine("You are outgoing and enjoy active pursuits.");
-        }
-        else
-        {
-            Console.WriteLine("You are creative and enjoy artistic endeavors.");
-        }
+        Console.WriteLine($"Fraction representation of {number}: {fraction}/{n}");
     }
 }
