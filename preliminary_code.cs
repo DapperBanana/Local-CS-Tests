950c91a38c159e3e5e586123784db5841f87e873
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-29 13:28:38 -0600

    Added support for user roles in the application

--- preliminary_code.cs
+++ preliminary_code.cs
@@ -1,74 +1,26 @@
 
 using System;
 
-class VirtualPet
-{
-    private string name;
-    private int hunger;
-    private int happiness;
-
-    public VirtualPet(string name)
-    {
-        this.name = name;
-        this.hunger = 50;
-        this.happiness = 50;
-    }
-
-    public void Feed()
-    {
-        hunger -= 10;
-        happiness += 5;
-        Console.WriteLine($"{name} has been fed and is now happier!");
-    }
-
-    public void Play()
-    {
-        hunger += 5;
-        happiness += 10;
-        Console.WriteLine($"{name} is having fun playing!");
-    }
-
-    public void GetStatus()
-    {
-        Console.WriteLine($"{name} - Hunger: {hunger}, Happiness: {happiness}");
-    }
-}
-
 class Program
 {
     static void Main()
     {
-        VirtualPet myPet = new VirtualPet("Fluffy");
-        bool exit = false;
+        Console.Write("Enter the number of terms in the Fibonacci sequence: ");
+        int n = int.Parse(Console.ReadLine());
 
-        while (!exit)
+        for (int i = 0; i < n; i++)
         {
-            Console.WriteLine("1. Feed Pet");
-            Console.WriteLine("2. Play with Pet");
-            Console.WriteLine("3. Get Pet's Status");
-            Console.WriteLine("4. Exit");
-
-            Console.Write("Choose an option: ");
-            int choice = int.Parse(Console.ReadLine());
-
-            switch (choice)
-            {
-                case 1:
-                    myPet.Feed();
-                    break;
-                case 2:
-                    myPet.Play();
-                    break;
-                case 3:
-                    myPet.GetStatus();
-                    break;
-                case 4:
-                    exit = true;
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Please try again.");
-                    break;
-            }
+            Console.Write(Fibonacci(i) + " ");
         }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
 }
