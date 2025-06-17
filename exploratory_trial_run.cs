e41a1b0c73b73fd3e36d38c3f66d709b0835fb07
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-16 18:00:45 -0600

    Fixed security vulnerability in module O

---
+++ exploratory_trial_run.cs
@@ -0,0 +1,28 @@
+
+using System;
+
+class PrimeFactors
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        Console.Write($"Prime factors of {number} are: ");
+        
+        for (int i = 2; i <= number; i++)
+        {
+            while (number % i == 0)
+            {
+                Console.Write(i + " ");
+                number = number / i;
+            }
+        }
+        
+        if (number > 1)
+        {
+            Console.Write(number);
+        }
+        Console.WriteLine();
+    }
+}
