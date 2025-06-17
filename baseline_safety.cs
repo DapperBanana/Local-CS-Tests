3cad6ea7fe8819e1538ee17f94d4193da82407d7
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-16 18:11:08 -0600

    Fixed issue with file uploads in feature II

--- baseline_safety.cs
+++ baseline_safety.cs
@@ -1,39 +1,56 @@
 
 using System;
 
-class CrosswordPuzzle
+class Program
 {
     static void Main()
     {
-        // Define the size of the crossword puzzle
-        int rows = 10;
-        int cols = 10;
+        int[,] matrix = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
 
-        // Create a 2D array to represent the crossword puzzle
-        char[,] puzzle = new char[rows, cols];
+        int determinant = CalculateDeterminant(matrix);
+        Console.WriteLine("Determinant of the matrix is: " + determinant);
+    }
 
-        // Define the alphabet for filling the crossword puzzle
-        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-
-        // Fill the crossword puzzle with random characters from the alphabet
-        Random rnd = new Random();
-        for (int i = 0; i < rows; i++)
+    static int CalculateDeterminant(int[,] matrix)
+    {
+        if (matrix.GetLength(0) != matrix.GetLength(1))
         {
-            for (int j = 0; j < cols; j++)
-            {
-                puzzle[i, j] = alphabet[rnd.Next(alphabet.Length)];
-            }
+            throw new ArgumentException("The matrix must be square.");
         }
 
-        // Display the crossword puzzle
-        Console.WriteLine("Crossword Puzzle:");
-        for (int i = 0; i < rows; i++)
+        if (matrix.GetLength(0) == 1)
         {
-            for (int j = 0; j < cols; j++)
-            {
-                Console.Write(puzzle[i, j] + " ");
-            }
-            Console.WriteLine();
+            return matrix[0, 0];
         }
+
+        int determinant = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            determinant += matrix[0, i] * (int)Math.Pow(-1, i) * CalculateDeterminant(GetSubMatrix(matrix, 0, i));
+        }
+
+        return determinant;
+    }
+
+    static int[,] GetSubMatrix(int[,] matrix, int rowToRemove, int colToRemove)
+    {
+        int size = matrix.GetLength(0) - 1;
+        int[,] subMatrix = new int[size, size];
+        for (int i = 0, m = 0; i < matrix.GetLength(0); i++)
+        {
+            if (i == rowToRemove) continue;
+            for (int j = 0, n = 0; j < matrix.GetLength(0); j++)
+            {
+                if (j == colToRemove) continue;
+                subMatrix[m, n] = matrix[i, j];
+                n++;
+            }
+            m++;
+        }
+        return subMatrix;
     }
 }
