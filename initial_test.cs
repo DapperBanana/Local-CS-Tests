66040ae2752dcfc0c9ce338305f6b86365ef4a83
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-23 11:55:59 -0600

    Optimized database schema for improved efficiency

---
+++ initial_test.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double radius, height;
+
+        Console.Write("Enter the radius of the cylinder: ");
+        radius = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the height of the cylinder: ");
+        height = double.Parse(Console.ReadLine());
+
+        double volume = Math.PI * Math.Pow(radius, 2) * height;
+
+        Console.WriteLine("The volume of the cylinder is: " + volume);
+    }
+}
