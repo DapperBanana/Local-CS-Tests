a0c87350fbb5325c59ec50fa83346e403ca833be
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-25 16:14:19 -0600

    Implemented data caching for faster retrieval

---
+++ alpha_sandbox.cs
@@ -0,0 +1,36 @@
+
+using System;
+
+class LongestIncreasingSubsequence
+{
+    static void Main()
+    {
+        int[] arr = { 10, 22, 9, 33, 21, 50, 41, 60 };
+        int n = arr.Length;
+
+        int[] lis = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            lis[i] = 1;
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                {
+                    lis[i] = lis[j] + 1;
+                }
+            }
+        }
+
+        int maxLength = 0;
+        for (int i = 0; i < n; i++)
+        {
+            maxLength = Math.Max(maxLength, lis[i]);
+        }
+
+        Console.WriteLine("The length of the longest increasing subsequence is: " + maxLength);
+    }
+}
