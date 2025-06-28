5d3155d59298068eead16bc1ed2c78d26ecbc743
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-28 11:30:32 -0600

    Upgraded project dependencies to meet security standards

---
+++ proof_of_concept.cs
@@ -0,0 +1,45 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the limit: ");
+        int limit = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = 2; i <= limit; i++)
+        {
+            if (IsPrime(i))
+            {
+                sum += i;
+            }
+        }
+
+        Console.WriteLine("The sum of all prime numbers up to {0} is: {1}", limit, sum);
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number == 2)
+        {
+            return true;
+        }
+
+        if (number < 2 || number % 2 == 0)
+        {
+            return false;
+        }
+
+        for (int i = 3; i <= Math.Sqrt(number); i += 2)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
