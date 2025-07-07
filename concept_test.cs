6e633c7d309222029931e5f4400871d64e04f374
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-06 19:18:51 -0600

    Changed styling for better readability

---
+++ concept_test.cs
@@ -0,0 +1,49 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = {
+            {1, 2, 3},
+            {2, 4, 5},
+            {3, 5, 6}
+        };
+
+        bool isSymmetric = CheckSymmetric(matrix);
+
+        if (isSymmetric)
+        {
+            Console.WriteLine("The given matrix is symmetric.");
+        }
+        else
+        {
+            Console.WriteLine("The given matrix is not symmetric.");
+        }
+    }
+
+    static bool CheckSymmetric(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (rows != cols)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (matrix[i,j] != matrix[j,i])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
