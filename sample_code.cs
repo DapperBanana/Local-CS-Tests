f79f4c5465c3f3029ab640e0d6aca750ffac033a
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-07 11:21:27 -0600

    Implemented feature GG based on customer request

---
+++ sample_code.cs
@@ -0,0 +1,53 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix1 = new int[,] { { 1, 2 }, { 3, 4 } };
+        int[,] matrix2 = new int[,] { { 5, 6 }, { 7, 8 } };
+
+        int[,] result = MultiplyMatrices(matrix1, matrix2);
+
+        Console.WriteLine("Result:");
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Console.Write(result[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+    {
+        int rows1 = matrix1.GetLength(0);
+        int cols1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int cols2 = matrix2.GetLength(1);
+
+        if (cols1 != rows2)
+        {
+            throw new Exception("Matrices cannot be multiplied");
+        }
+
+        int[,] result = new int[rows1, cols2];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < cols2; j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < cols1; k++)
+                {
+                    sum += matrix1[i, k] * matrix2[k, j];
+                }
+                result[i, j] = sum;
+            }
+        }
+
+        return result;
+    }
+}
