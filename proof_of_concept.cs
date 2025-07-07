1cdb2a5208f1cd88057c5fcab1d2976db348a7d1
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-07 11:13:29 -0600

    Refactored code in module A

--- proof_of_concept.cs
+++ proof_of_concept.cs
@@ -1,24 +1,41 @@
 
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Media.Imaging;
 
-class Program
+namespace ImageProcessing
 {
-    static void Main()
+    class Program
     {
-        // Prompt user for input
-        Console.WriteLine("Enter the radius of the top of the frustum:");
-        double radius1 = Convert.ToDouble(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            // Load the image
+            Bitmap image = new Bitmap("input.jpg");
+            
+            // Convert the image to grayscale
+            Bitmap grayscaleImage = ToGrayscale(image);
+            
+            // Save the grayscale image
+            grayscaleImage.Save("output.jpg", ImageFormat.Jpeg);
+        }
 
-        Console.WriteLine("Enter the radius of the base of the frustum:");
-        double radius2 = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("Enter the slant height of the frustum:");
-        double height = Convert.ToDouble(Console.ReadLine());
-
-        // Calculate the area of the frustum
-        double area = Math.PI * (radius1 + radius2) * (radius1 + Math.Sqrt(Math.Pow(height, 2) + Math.Pow(radius1 - radius2, 2)));
-
-        // Display the area of the frustum
-        Console.WriteLine("The area of the frustum of the cone is: " + area);
+        static Bitmap ToGrayscale(Bitmap image)
+        {
+            Bitmap grayscaleImage = new Bitmap(image.Width, image.Height);
+            
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color pixel = image.GetPixel(x, y);
+                    int gray = (int)(pixel.R * 0.299 + pixel.G * 0.587 + pixel.B * 0.114);
+                    Color grayPixel = Color.FromArgb(gray, gray, gray);
+                    grayscaleImage.SetPixel(x, y, grayPixel);
+                }
+            }
+            
+            return grayscaleImage;
+        }
     }
 }
