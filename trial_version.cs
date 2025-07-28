bb0704d3aa8c77ba50cd6c180651805c40254ebd
Austin L. Howard <austin.lane.howard@gmail.com>
2025-07-27 18:17:11 -0600

    Fixed issue with file uploads in feature II

---
+++ trial_version.cs
@@ -0,0 +1,21 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> list1 = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> list2 = new List<int> { 3, 4, 5, 6, 7 };
+
+        IEnumerable<int> intersection = list1.Intersect(list2);
+
+        Console.WriteLine("Intersection of list1 and list2:");
+        foreach (int num in intersection)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
