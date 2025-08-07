87f1a77d045a01a69c79a7c9ef355c3580c3cea7
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-07 13:37:05 -0600

    Integrated unit test coverage for critical paths

---
+++ functionality_check.cs
@@ -0,0 +1,29 @@
+
+using System;
+
+class DecimalToBinaryConverter
+{
+    static void Main()
+    {
+        Console.Write("Enter a decimal number: ");
+        int decimalNumber = int.Parse(Console.ReadLine());
+
+        string binaryNumber = ConvertDecimalToBinary(decimalNumber);
+
+        Console.WriteLine($"Binary equivalent: {binaryNumber}");
+    }
+
+    static string ConvertDecimalToBinary(int decimalNumber)
+    {
+        string binaryNumber = "";
+
+        while (decimalNumber > 0)
+        {
+            int remainder = decimalNumber % 2;
+            binaryNumber = remainder + binaryNumber;
+            decimalNumber = decimalNumber / 2;
+        }
+
+        return binaryNumber;
+    }
+}
