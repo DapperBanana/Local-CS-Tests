4f5e6a3806e85d97e057a9a793e9f30c1bed107c
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-15 14:07:11 -0600

    Implemented user permission checks in module JJ

---
+++ diagnostic_check.cs
@@ -0,0 +1,17 @@
+
+using System;
+
+class TemperatureConverter
+{
+    static void Main()
+    {
+        double celsius, fahrenheit;
+
+        Console.Write("Enter temperature in Celsius: ");
+        celsius = Convert.ToDouble(Console.ReadLine());
+
+        fahrenheit = celsius * 9 / 5 + 32;
+
+        Console.WriteLine("Temperature in Fahrenheit: " + fahrenheit);
+    }
+}
