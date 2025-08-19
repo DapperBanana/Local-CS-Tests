40dcaa8000752e3a4b63de4c6ea4d3032949e905
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-18 18:06:49 -0600

    Fixed edge case scenario in module U

---
+++ pilot_code.cs
@@ -0,0 +1,28 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        int sum = CalculateSumOfDigits(number);
+        
+        Console.WriteLine("Sum of digits: " + sum);
+    }
+    
+    static int CalculateSumOfDigits(int number)
+    {
+        int sum = 0;
+        
+        while (number != 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        
+        return sum;
+    }
+}
