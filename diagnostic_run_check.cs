9fcf1033ac9fd101a2a17e5332719e375bbf684c
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-22 16:41:11 -0600

    Integrated feedback from user testing

---
+++ diagnostic_run_check.cs
@@ -0,0 +1,22 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+        
+        string reversedString = ReverseString(input);
+        
+        Console.WriteLine("Reversed string: " + reversedString);
+    }
+
+    static string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
