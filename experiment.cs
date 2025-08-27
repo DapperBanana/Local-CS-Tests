442452d286edc23ed5e4586f695f4eecd56cf282
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-27 17:52:33 -0600

    Updated project logo and branding

---
+++ experiment.cs
@@ -0,0 +1,15 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the temperature in Celsius:");
+        double celsius = double.Parse(Console.ReadLine());
+
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        Console.WriteLine("The temperature in Fahrenheit is: " + fahrenheit);
+    }
+}
