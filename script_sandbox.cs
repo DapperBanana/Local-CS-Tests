52f1fb014b2f59469f6eb7860ad1894c19193fcd
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-02 18:45:01 -0600

    Applied coding standards to maintain consistency

---
+++ script_sandbox.cs
@@ -0,0 +1,33 @@
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Hello, World!";
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        foreach (char c in input)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+        }
+
+        Console.WriteLine("Character Count:");
+        foreach (var pair in charCount)
+        {
+            Console.WriteLine(pair.Key + " : " + pair.Value);
+        }
+    }
+}
