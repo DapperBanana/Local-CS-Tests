42b5d9853036b8f396f8453d4942d0417118b1c9
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-06 12:45:21 -0600

    Implemented user permission checks in module JJ

---
+++ safety_check.cs
@@ -0,0 +1,19 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a positive integer n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+
+        Console.WriteLine($"The sum of the first {n} natural numbers is: {sum}");
+    }
+}
