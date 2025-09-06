622542039f136bffabc6657a8830c26f91ffe3c2
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-06 12:42:52 -0600

    Fixed race condition in asynchronous tasks

--- validation.cs
+++ validation.cs
@@ -5,16 +5,31 @@ class Program
 {
     static void Main()
     {
-        double x1 = 1, y1 = 2, z1 = 3; // coordinates of point 1
-        double x2 = 4, y2 = 5, z2 = 6; // coordinates of point 2
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
 
-        double distance = CalculateDistance(x1, y1, z1, x2, y2, z2);
-        Console.WriteLine("The distance between the two points is: " + distance);
+        if (IsPalindrome(number))
+        {
+            Console.WriteLine($"{number} is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is not a palindrome.");
+        }
     }
 
-    static double CalculateDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+    static bool IsPalindrome(int number)
     {
-        double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-        return distance;
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number > 0)
+        {
+            int remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number = number / 10;
+        }
+
+        return originalNumber == reversedNumber;
     }
 }
