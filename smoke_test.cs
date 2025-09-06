0154f3535de9bfaaf599427d4da15a6ab166916a
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-06 12:30:35 -0600

    Refactored code to improve code readability

---
+++ smoke_test.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+class LeapYearChecker
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            Console.WriteLine("{0} is a leap year.", year);
+        }
+        else
+        {
+            Console.WriteLine("{0} is not a leap year.", year);
+        }
+    }
+}
