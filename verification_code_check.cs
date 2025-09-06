fe93e67bc52f9e9d15441ae203da18062e26b55d
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-06 12:41:19 -0600

    Refactored code to follow design patterns in module BB

---
+++ verification_code_check.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+class EvenOdd
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("The number is even.");
+        }
+        else
+        {
+            Console.WriteLine("The number is odd.");
+        }
+    }
+}
