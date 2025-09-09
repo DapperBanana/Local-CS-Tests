c4b4d81b7094fd8eb11b54adeb864b03de5bbb6d
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-09 11:27:48 -0600

    Integrated support for push notifications

--- preliminary_suite.cs
+++ preliminary_suite.cs
@@ -5,12 +5,63 @@ class Program
 {
     static void Main()
     {
-        double sideLength;
-        Console.Write("Enter the side length of the icosahedron: ");
-        sideLength = Convert.ToDouble(Console.ReadLine());
+        int n = 3; // Size of the square matrix
+        int[,] matrix = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
 
-        double area = 5 * Math.Sqrt(3) * Math.Pow(sideLength, 2);
+        Console.WriteLine("Matrix:");
+        PrintMatrix(matrix, n);
 
-        Console.WriteLine($"The area of the regular icosahedron is: {area}");
+        int det = CalculateDeterminant(matrix, n);
+        Console.WriteLine("Determinant: " + det);
+    }
+
+    static int CalculateDeterminant(int[,] matrix, int n)
+    {
+        if (n == 1) // Base case
+        {
+            return matrix[0, 0];
+        }
+
+        int determinant = 0;
+        int sign = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[,] subMatrix = new int[n - 1, n - 1];
+
+            for (int row = 1; row < n; row++)
+            {
+                int colIndex = 0;
+                for (int col = 0; col < n; col++)
+                {
+                    if (col != i)
+                    {
+                        subMatrix[row - 1, colIndex] = matrix[row, col];
+                        colIndex++;
+                    }
+                }
+            }
+
+            determinant += sign * matrix[0, i] * CalculateDeterminant(subMatrix, n - 1);
+            sign *= -1;
+        }
+
+        return determinant;
+    }
+
+    static void PrintMatrix(int[,] matrix, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
