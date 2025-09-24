17ec0094c7a3f757710839711a76a51e626a9a34
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-24 15:01:35 -0600

    Improved user interface for feature J

---
+++ trial_run.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double radius, height, volume;
+        
+        Console.WriteLine("Enter the radius of the cylinder: ");
+        radius = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Enter the height of the cylinder: ");
+        height = Convert.ToDouble(Console.ReadLine());
+        
+        volume = Math.PI * Math.Pow(radius, 2) * height;
+        
+        Console.WriteLine($"The volume of the cylinder is: {volume}");
+    }
+}
