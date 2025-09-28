946c02be5b228702cf3e139f5fb513a461376085
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-28 13:52:52 -0600

    Applied coding standards to maintain consistency

--- code_probe.cs
+++ code_probe.cs
@@ -1,43 +1,43 @@
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        int startRange, endRange;
-        
-        Console.Write("Enter the start of the range: ");
-        startRange = int.Parse(Console.ReadLine());
-        
-        Console.Write("Enter the end of the range: ");
-        endRange = int.Parse(Console.ReadLine());
-        
-        int sum = 0;
-        
-        for (int num = startRange; num <= endRange; num++)
-        {
-            if (IsPrime(num))
-            {
-                sum += num;
-            }
-        }
-        
-        Console.WriteLine($"The sum of all prime numbers in the range {startRange} to {endRange} is: {sum}");
+        List<double> list1 = new List<double> { 1, 2, 3, 4, 5 };
+        List<double> list2 = new List<double> { 2, 3, 4, 5, 6 };
+
+        double correlation = CalculateCorrelation(list1, list2);
+        Console.WriteLine($"Correlation coefficient: {correlation}");
     }
-    
-    static bool IsPrime(int num)
+
+    static double CalculateCorrelation(List<double> list1, List<double> list2)
     {
-        if (num < 2) return false;
-        
-        for (int i = 2; i <= Math.Sqrt(num); i++)
+        if (list1.Count != list2.Count)
         {
-            if (num % i == 0)
-            {
-                return false;
-            }
+            throw new ArgumentException("Lists must have the same length.");
         }
-        
-        return true;
+
+        double avg1 = list1.Average();
+        double avg2 = list2.Average();
+
+        double numerator = 0;
+        double denom1 = 0;
+        double denom2 = 0;
+
+        for (int i = 0; i < list1.Count; i++)
+        {
+            double val1 = list1[i] - avg1;
+            double val2 = list2[i] - avg2;
+
+            numerator += val1 * val2;
+            denom1 += val1 * val1;
+            denom2 += val2 * val2;
+        }
+
+        return numerator / (Math.Sqrt(denom1) * Math.Sqrt(denom2));
     }
 }
