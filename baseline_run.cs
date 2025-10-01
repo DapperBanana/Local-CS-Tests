3759f40cecfa6d89e73e02a05724ed7c566baf96
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-01 12:02:40 -0600

    Synchronized data models with API changes

--- baseline_run.cs
+++ baseline_run.cs
@@ -1,43 +1,59 @@
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Rasterio;
 
-class BaseConverter
+class Program
 {
     static void Main()
     {
-        Console.Write("Enter the number to convert: ");
-        string number = Console.ReadLine();
+        string filePath = "path_to_your_satellite_image.tif";
 
-        Console.Write("Enter the base it is currently in: ");
-        int fromBase = int.Parse(Console.ReadLine());
-
-        Console.Write("Enter the base you want to convert it to: ");
-        int toBase = int.Parse(Console.ReadLine());
-
-        int decimalNumber = ConvertToDecimal(number, fromBase);
-        string convertedNumber = ConvertFromDecimal(decimalNumber, toBase);
-
-        Console.WriteLine($"Converted number: {convertedNumber}");
-    }
-
-    static int ConvertToDecimal(string number, int fromBase)
-    {
-        int result = 0;
-        for (int i = 0; i < number.Length; i++)
+        using (var dataset = Dataset.Open(filePath))
         {
-            result += (int)Math.Pow(fromBase, i) * int.Parse(number[number.Length - 1 - i].ToString());
-        }
-        return result;
-    }
+            if (dataset != null)
+            {
+                Band greenBand = dataset.Bands.FirstOrDefault(band => band.Name.Contains("green"));
+                Band redBand = dataset.Bands.FirstOrDefault(band => band.Name.Contains("red"));
 
-    static string ConvertFromDecimal(int number, int toBase)
-    {
-        string result = "";
-        while (number > 0)
-        {
-            result = (number % toBase) + result;
-            number /= toBase;
+                if (greenBand != null && redBand != null)
+                {
+                    double totalCount = 0.0;
+                    double vegetationCount = 0.0;
+
+                    for (int row = 0; row < dataset.Height; row++)
+                    {
+                        for (int col = 0; col < dataset.Width; col++)
+                        {
+                            double greenValue = greenBand.ReadPixel(row, col)[0];
+                            double redValue = redBand.ReadPixel(row, col)[0];
+
+                            if (greenValue > 0 && redValue > 0)
+                            {
+                                double ndvi = (greenValue - redValue) / (greenValue + redValue);
+
+                                totalCount++;
+                                if (ndvi > 0.2) // threshold for vegetation
+                                {
+                                    vegetationCount++;
+                                }
+                            }
+                        }
+                    }
+
+                    double vegetationPercentage = (vegetationCount / totalCount) * 100;
+                    Console.WriteLine($"Vegetation percentage: {vegetationPercentage}%");
+                }
+                else
+                {
+                    Console.WriteLine("Could not find green and red bands in the raster dataset.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed to open raster dataset.");
+            }
         }
-        return result;
     }
 }
