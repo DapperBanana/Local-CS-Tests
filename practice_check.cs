ae77af5e2fb5a7364537c6211e186c0810bc7338
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-02 13:04:09 -0600

    Implemented data caching for faster retrieval

--- practice_check.cs
+++ practice_check.cs
@@ -3,39 +3,52 @@ using System;
 
 class Program
 {
+    public static bool IsVampireNumber(int number)
+    {
+        string numberStr = number.ToString();
+        int digits = numberStr.Length;
+        if (digits % 2 != 0)
+        {
+            return false;
+        }
+
+        for (int i = 10; i < 100; i++)
+        {
+            if (numberStr.Contains(i.ToString()))
+            {
+                int factor1 = i;
+                int factor2 = number / i;
+                if (factor1 * factor2 == number)
+                {
+                    string resultStr = factor1.ToString() + factor2.ToString();
+                    char[] resultArray = resultStr.ToCharArray();
+                    Array.Sort(resultArray);
+
+                    string numberSorted = String.Concat(numberStr.OrderBy(c => c));
+
+                    if (String.Join("", resultArray) == numberSorted)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
     static void Main()
     {
-        Console.Write("Enter a number: ");
+        Console.WriteLine("Enter a number to check if it is a vampire number: ");
         int number = int.Parse(Console.ReadLine());
 
-        if (IsPerfectDigitalInvariant(number))
+        if (IsVampireNumber(number))
         {
-            Console.WriteLine($"{number} is a perfect digital invariant.");
+            Console.WriteLine(number + " is a vampire number.");
         }
         else
         {
-            Console.WriteLine($"{number} is not a perfect digital invariant.");
+            Console.WriteLine(number + " is not a vampire number.");
         }
-    }
-
-    static bool IsPerfectDigitalInvariant(int number)
-    {
-        int originalNumber = number;
-        int reversedNumber = ReverseNumber(number);
-        int sum = originalNumber + reversedNumber;
-
-        return sum == originalNumber;
-    }
-
-    static int ReverseNumber(int number)
-    {
-        int reversed = 0;
-        while (number > 0)
-        {
-            int remainder = number % 10;
-            reversed = reversed * 10 + remainder;
-            number = number / 10;
-        }
-        return reversed;
     }
 }
