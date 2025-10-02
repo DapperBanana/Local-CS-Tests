d52b5d7f5d40fbf79a06828b6aa8f75e90215a27
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-02 13:10:01 -0600

    Integrated unit test coverage for critical paths

--- exploratory_probe.cs
+++ exploratory_probe.cs
@@ -1,36 +1,15 @@
 
 using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        List<int> numbers = new List<int> { 1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 9, 9 };
+        Console.Write("Enter the side length of the octagon: ");
+        double sideLength = Convert.ToDouble(Console.ReadLine());
 
-        List<int> uniqueNumbers = new List<int>();
-        HashSet<int> seen = new HashSet<int>();
+        double area = 2 * (1 + Math.Sqrt(2)) * Math.Pow(sideLength, 2);
 
-        foreach (var number in numbers)
-        {
-            if (seen.Add(number))
-            {
-                uniqueNumbers.Add(number);
-            }
-        }
-
-        Console.WriteLine("Original List:");
-        PrintList(numbers);
-
-        Console.WriteLine("\nList with Duplicates Removed:");
-        PrintList(uniqueNumbers);
-    }
-
-    static void PrintList(List<int> list)
-    {
-        foreach (var item in list)
-        {
-            Console.Write(item + " ");
-        }
+        Console.WriteLine($"The area of the regular octagon is: {area}");
     }
 }
