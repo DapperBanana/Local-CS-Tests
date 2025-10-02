c36e4d8e7b0750942ee3f5aaeb00b52c37ad61be
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-02 13:04:43 -0600

    Implemented user authentication for improved security

---
+++ verification_code_run.cs
@@ -0,0 +1,38 @@
+
+using System;
+
+class Program
+{
+    static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        for (int i = 3; i <= Math.Sqrt(number); i += 2)
+        {
+            if (number % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int input = int.Parse(Console.ReadLine());
+
+        int num = input + 1;
+
+        while (true)
+        {
+            if (IsPrime(num))
+            {
+                Console.WriteLine($"The smallest prime number greater than {input} is: {num}");
+                break;
+            }
+
+            num++;
+        }
+    }
+}
