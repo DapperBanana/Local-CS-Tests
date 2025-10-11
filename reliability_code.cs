2c2889d401bbfd2d972c6fc60c02b180f441dad3
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-11 12:49:29 -0600

    Integrated automated code formatting

--- reliability_code.cs
+++ reliability_code.cs
@@ -1,32 +1,25 @@
 
 using System;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Enter a string:");
-        string input = Console.ReadLine();
+        double radius1, radius2, height, slantHeight, area;
 
-        Dictionary<char, int> charCounts = new Dictionary<char, int>();
+        Console.WriteLine("Enter the radius of the larger base of the frustum: ");
+        radius1 = Convert.ToDouble(Console.ReadLine());
 
-        foreach (char c in input)
-        {
-            if (charCounts.ContainsKey(c))
-            {
-                charCounts[c]++;
-            }
-            else
-            {
-                charCounts[c] = 1;
-            }
-        }
+        Console.WriteLine("Enter the radius of the smaller base of the frustum: ");
+        radius2 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("Character Counts:");
-        foreach (var pair in charCounts)
-        {
-            Console.WriteLine($"{pair.Key} - {pair.Value}");
-        }
+        Console.WriteLine("Enter the height of the frustum: ");
+        height = Convert.ToDouble(Console.ReadLine());
+
+        slantHeight = Math.Sqrt(Math.Pow((radius1 - radius2), 2) + Math.Pow(height, 2));
+
+        area = Math.PI * (radius1 + radius2) * slantHeight + Math.PI * Math.Pow(radius1, 2) + Math.PI * Math.Pow(radius2, 2);
+
+        Console.WriteLine("The area of the frustum of a cone is: " + area);
     }
 }
