a1529c34437a64543b9063d801fbc17426d5555e
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-10 18:55:44 -0600

    Renamed variable G for clarity

---
+++ code_safety_check.cs
@@ -0,0 +1,34 @@
+
+using System;
+
+class Program
+{
+    static int HammingDistance(string str1, string str2)
+    {
+        if (str1.Length != str2.Length)
+        {
+            throw new ArgumentException("Strings must be of equal length");
+        }
+
+        int distance = 0;
+        for (int i = 0; i < str1.Length; i++)
+        {
+            if (str1[i] != str2[i])
+            {
+                distance++;
+            }
+        }
+
+        return distance;
+    }
+
+    static void Main()
+    {
+        string str1 = "karolin";
+        string str2 = "kathrin";
+
+        int distance = HammingDistance(str1, str2);
+
+        Console.WriteLine($"Hamming distance between '{str1}' and '{str2}' is {distance}");
+    }
+}
