7263a9c458956e675a06d01d1106a820172b11d0
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-12 16:14:34 -0600

    Refactored code to follow design patterns in module BB

---
+++ prototype_run.cs
@@ -0,0 +1,38 @@
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first string:");
+        string str1 = Console.ReadLine();
+        
+        Console.WriteLine("Enter the second string:");
+        string str2 = Console.ReadLine();
+        
+        List<char> commonChars = FindCommonCharacters(str1, str2);
+        
+        Console.WriteLine("Common characters:");
+        foreach (char ch in commonChars)
+        {
+            Console.Write(ch + " ");
+        }
+    }
+    
+    static List<char> FindCommonCharacters(string str1, string str2)
+    {
+        List<char> commonChars = new List<char>();
+        
+        foreach (char ch in str1)
+        {
+            if (str2.Contains(ch) && !commonChars.Contains(ch))
+            {
+                commonChars.Add(ch);
+            }
+        }
+        
+        return commonChars;
+    }
+}
