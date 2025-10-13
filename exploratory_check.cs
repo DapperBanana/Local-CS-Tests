e6dac5a025a9fb24c13eee253e199b7b5019badf
Austin L. Howard <austin.lane.howard@gmail.com>
2025-10-13 15:49:32 -0600

    Optimized database queries in module V

---
+++ exploratory_check.cs
@@ -0,0 +1,49 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = new int[,]
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        int[,] reversedMatrix = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                reversedMatrix[i, j] = matrix[rows - i - 1, cols - j - 1];
+            }
+        }
+
+        Console.WriteLine("Original matrix:");
+        PrintMatrix(matrix);
+
+        Console.WriteLine("\nReversed matrix:");
+        PrintMatrix(reversedMatrix);
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
