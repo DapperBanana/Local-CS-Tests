50875ee91336ab797c8d62facc4f945d32b77c8e
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-05 13:02:43 -0700

    Enhanced error messages for better user understanding

---
+++ scenario_test.cs
@@ -0,0 +1,30 @@
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 10, 20, 5, 15, 25 };
+
+        int largest = FindLargestElement(numbers);
+
+        Console.WriteLine("The largest element in the list is: " + largest);
+    }
+
+    static int FindLargestElement(List<int> numbers)
+    {
+        int largest = Int32.MinValue;
+
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        return largest;
+    }
+}
