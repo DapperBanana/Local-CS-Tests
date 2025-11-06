1ae2f5f587966adc7704838f2b889a48f0435939
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-06 12:09:23 -0700

    Fixed memory leaks in feature FF

---
+++ alpha_check.cs
@@ -0,0 +1,48 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        int[,] reversedMatrix = new int[cols, rows];
+
+        // Reversing the order of rows and columns
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                reversedMatrix[j, i] = matrix[i, j];
+            }
+        }
+
+        // Displaying the original matrix
+        Console.WriteLine("Original Matrix:");
+        PrintMatrix(matrix);
+
+        // Displaying the reversed matrix
+        Console.WriteLine("\nReversed Matrix:");
+        PrintMatrix(reversedMatrix);
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
