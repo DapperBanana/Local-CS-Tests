5f6d6df9078bd9c5d997b6313785de6090b22bb2
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-16 11:42:14 -0700

    Fixed memory leaks in feature FF

---
+++ exploratory_script.cs
@@ -0,0 +1,48 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        int[,] reversedMatrix = new int[cols, rows];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                reversedMatrix[j, i] = matrix[i, j];
+            }
+        }
+
+        Console.WriteLine("Original Matrix:");
+        PrintMatrix(matrix);
+
+        Console.WriteLine("\nReversed Matrix:");
+        PrintMatrix(reversedMatrix);
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
