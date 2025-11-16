497cf1ba259d2da5e06f0271c36920773e0d6db0
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-15 17:10:02 -0700

    Implemented feature GG based on customer request

--- script_trial.cs
+++ script_trial.cs
@@ -1,57 +1,49 @@
 
 using System;
 
-class Stack
-{
-    private int[] items;
-    private int top;
-    
-    public Stack(int size)
-    {
-        items = new int[size];
-        top = -1;
-    }
-    
-    public void Push(int item)
-    {
-        if (top == items.Length - 1)
-        {
-            Console.WriteLine("Stack overflow");
-        }
-        else
-        {
-            items[++top] = item;
-        }
-    }
-    
-    public int Pop()
-    {
-        if (top == -1)
-        {
-            Console.WriteLine("Stack underflow");
-            return -1;
-        }
-        else
-        {
-            return items[top--];
-        }
-    }
-}
-
 class Program
 {
     static void Main()
     {
-        Stack stack = new Stack(5);
-        
-        stack.Push(1);
-        stack.Push(2);
-        stack.Push(3);
-        
-        Console.WriteLine(stack.Pop()); // 3
-        Console.WriteLine(stack.Pop()); // 2
-        Console.WriteLine(stack.Pop()); // 1
-        Console.WriteLine(stack.Pop()); // Stack underflow
-        
+        int cars = 0;
+        int tick = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Tick: " + tick);
+            Console.WriteLine("There are currently " + cars + " cars on the road.");
+
+            string input = Console.ReadLine();
+
+            if (input == "add")
+            {
+                cars++;
+                Console.WriteLine("Added a car to the road.");
+            }
+            else if (input == "remove")
+            {
+                if (cars > 0)
+                {
+                    cars--;
+                    Console.WriteLine("Removed a car from the road.");
+                }
+                else
+                {
+                    Console.WriteLine("There are no cars on the road to remove.");
+                }
+            }
+            else if (input == "exit")
+            {
+                Console.WriteLine("Exiting traffic simulation program.");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid command. Please type 'add' to add a car, 'remove' to remove a car, or 'exit' to exit the program.");
+            }
+
+            tick++;
+            Console.WriteLine();
+        }
     }
 }
