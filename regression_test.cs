92973024cd4ea4fabaf1277ffece968250818551
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-15 17:00:16 -0700

    Adjusted API response format for better compatibility

---
+++ regression_test.cs
@@ -0,0 +1,35 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Job Application System");
+        
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.Write("Enter your desired position: ");
+        string position = Console.ReadLine();
+        
+        Console.Write("Enter your years of experience: ");
+        int yearsOfExperience = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("\nThank you for applying!");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Desired Position: " + position);
+        Console.WriteLine("Years of Experience: " + yearsOfExperience);
+        
+        if (yearsOfExperience >= 2)
+        {
+            Console.WriteLine("Congratulations! You meet the minimum experience requirement.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you do not meet the minimum experience requirement.");
+        }
+        
+        Console.WriteLine("\nGoodbye!");
+    }
+}
