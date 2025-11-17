6cab5df6aa3fdde4944b34c5f33dad94c65ed3bc
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-17 14:30:58 -0700

    Updated project logo and branding

---
+++ mock_run.cs
@@ -0,0 +1,23 @@
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 20;
+
+        Console.WriteLine("Before swapping:");
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine("After swapping:");
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+    }
+}
